a4778a1ef0bd8d3c8ee3196fb02febf69748a358
Leah Grace <Leahgdg@hotmail.com>
2022-04-04 13:10:30 -0400

    "Dynamic Get requests with sorted results"

--- src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/controllers/DeveloperController.java
+++ src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/controllers/DeveloperController.java
@@ -3,7 +3,11 @@ package com.LeahGrace.MySQLDriverSpringBootApi.controllers;
 import com.LeahGrace.MySQLDriverSpringBootApi.models.Developer;
 import com.LeahGrace.MySQLDriverSpringBootApi.repositories.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -19,10 +23,38 @@ public class DeveloperController {
         return repository.findAll();
     }
 
+    @GetMapping("/cohort/{cohort}")
+    public ResponseEntity<List<Developer>> getDevelopersByCohort(@PathVariable Integer cohort) {
+//        repository.findByCohortAndLanguage(Sort.by("name"), 11, "Java");
+        return new ResponseEntity<>(repository.findAllByCohort(cohort, Sort.by("name")), HttpStatus.OK);
+    }
+
     @PostMapping
-    public @ResponseBody Developer createDeveloper(@RequestBody Developer newDeveloper){
-       // newDeveloper = repository.save(newDeveloper);
-        return (Developer) repository.save(newDeveloper);
+    public ResponseEntity<Developer> createDeveloper(@RequestBody Developer newDeveloper){
+        // newDeveloper = repository.save(newDeveloper);
+        return new ResponseEntity<>(repository.save(newDeveloper), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public @ResponseBody Developer getOneDeveloper(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Developer updateDeveloper(@PathVariable Long id, @RequestBody Developer updates) {
+        Developer developer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (updates.getName() != null) developer.setName(updates.getName());
+        if (updates.getEmail() != null) developer.setEmail(updates.getEmail());
+        if (updates.getCohort() != null) developer.setCohort(updates.getCohort());
+        if (updates.getLanguages() != null) developer.setLanguages(updates.getLanguages());
+
+        return repository.save(developer);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyDeveloper(@PathVariable Long id){
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 
 }
--- src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/repositories/DeveloperRepository.java
+++ src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/repositories/DeveloperRepository.java
@@ -1,7 +1,16 @@
 package com.LeahGrace.MySQLDriverSpringBootApi.repositories;
 
 import com.LeahGrace.MySQLDriverSpringBootApi.models.Developer;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+
+    List<Developer> findAllByCohort(Integer cohort, Sort sort);
+
+//    @Query("SELECT * FROM developer WHERE cohort = ?1 AND ?2 in languages")
+//    Developer findByCohortAndLanguage(Sort sort, Integer cohort, String language);
 }
