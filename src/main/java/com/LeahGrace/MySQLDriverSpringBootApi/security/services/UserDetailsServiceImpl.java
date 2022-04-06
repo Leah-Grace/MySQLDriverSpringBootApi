3f92ba025d5f43ea9883f46a6a3edef655a1f462
Leah Grace <Leahgdg@hotmail.com>
2022-04-05 20:36:59 -0400

    "Implementing aunthenticationJwtTokenFilter w userDetailService and AuthEntryPointJwt"

---
+++ src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.LeahGrace.MySQLDriverSpringBootApi.security.services;
+
+import com.LeahGrace.MySQLDriverSpringBootApi.models.auth.User;
+import com.LeahGrace.MySQLDriverSpringBootApi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUsersByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(("User Not Found: " + username)));
+
+        //we have a user
+        return UserDetailsImpl.build(user);
+    }
+
+}
