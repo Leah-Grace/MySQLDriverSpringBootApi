7f143043d6e182d5b1251e3819efaaeb5517020c
Leah Grace <Leahgdg@hotmail.com>
2022-04-14 22:17:43 -0400

    "news api GET request"

--- src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/controllers/TestController.java
+++ src/main/java/com/LeahGrace/MySQLDriverSpringBootApi/controllers/TestController.java
@@ -1,15 +1,26 @@
 package com.LeahGrace.MySQLDriverSpringBootApi.controllers;
 
+import com.LeahGrace.MySQLDriverSpringBootApi.payloads.api.response.Article;
+import com.LeahGrace.MySQLDriverSpringBootApi.payloads.api.response.NewsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("api/test")
 public class TestController {
+
+    @Value("${LeahGrace.MySQLDriverSpringBootApi.newsApiKey}")
+    private String apiKey;
+
+    @Autowired
+    RestTemplate restTemplate;  //Interfaces with external RESTapi's
 
     @GetMapping("/all")
     public String allAccess() {
@@ -32,6 +43,17 @@ public class TestController {
     @PreAuthorize("hasRole('ADMIN')")
     public String adminAccess() {
         return "Admin content";
+    }
+
+    @GetMapping("/news/{q}")
+    public ResponseEntity<?> getNewsArticles(@PathVariable String q) {
+        String uri = "https://newsapi.org/v2/everything?sortBy=popularity&apiKey=" + apiKey + "&q=" + q;
+
+        NewsResponse response = restTemplate.getForObject(uri,NewsResponse.class);
+
+        return ResponseEntity.ok(response.getArticles());
+
+
     }
 }
 
